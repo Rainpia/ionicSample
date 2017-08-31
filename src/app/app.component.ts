f549fdd7871538f694f955886ab3d562c6e9208f
ryp <yinrunping@appvantage.com.cn>
2017-08-31 17:56:08 +0800

    replace page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { ListsPage } from '../pages/lists/lists';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = ListsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ src/pages/lists/lists.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'lists.html'
+})
+export class ListsPage {
+
+  constructor() {
+
+  }
+}
