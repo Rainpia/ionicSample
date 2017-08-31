a34270880613930cd7d88527b76244804f01e357
ryp <yinrunping@appvantage.com.cn>
2017-08-31 18:20:15 +0800

    list get data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { Http, HttpModule } from "@angular/http";
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -15,6 +16,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/lists/lists.ts
+++ src/pages/lists/lists.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+
 
 @Component({
   templateUrl: 'lists.html'
 })
 export class ListsPage {
+  lists:Array<{id:number,label:string}>;
+  constructor(private http:Http) {
+    this.getList();
+    
+  }
 
-  constructor() {
-
+  getList (){
+    this.http
+      .get('assets/data/list.json')
+      .subscribe((result)=>{
+        this.lists = result.json();
+      });
   }
 }
