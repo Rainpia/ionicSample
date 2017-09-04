6a05d3379a6f55642cbf4501fa14a5943d5912a9
ryp <yinrunping@appvantage.com.cn>
2017-09-04 16:37:11 +0800

    cmd

--- run.js
+++ run.js
@@ -3,15 +3,20 @@ var sh = require('shelljs');
 var cmd1 = `ionic cordova build android`;
 sh.exec(cmd1);
 console.log('android build success');
-//build ios
-var cmd2 = `ionic cordova build ios`;
-sh.exec(cmd2);
-console.log('ios build success');
+
 // copy apk to temp download folder
 sh.rm('-rf', 'temp-download/android-debug.apk');
 sh.cp('-R', 'platforms/android/build/outputs/apk/android-debug.apk', 'temp-download');
 console.log('android copy apk success');
-sh.rm('-rf', 'temp-download/ionicSample.apk');
+
+//build ios
+sh.rm('-rf', 'platforms/ios/build');
+var cmd2 = `ionic cordova build ios`;
+sh.exec(cmd2);
+console.log('ios build success');
+
+
+sh.rm('-rf', 'temp-download/ionicSample.ipa/ionicSample.ipa');
 // build ios to archive
 var cmd3 =  `xcodebuild -scheme ionicSample clean archive -archivePath platforms/ios/build/ionicSample`;
 // generate ipa from archive
@@ -22,7 +27,13 @@ sh.exec(cmd4);
 console.log('ios generate ipa success');
 
 //deploy install file
-var cmd5 = `scp -r temp-download/** dan@192.168.1.43:/Users/dan/docker/www/temp-download`;
+var cmd5 = `ssh dan@192.168.1.43 "rm -f /Users/dan/docker/www/temp-download"`;
+var cmd6 = `ssh dan@192.168.1.43 "mkdir -p /Users/dan/docker/www/temp-download"`;
+var cmd7 = `scp -r temp-download/** dan@192.168.1.43:/Users/dan/docker/www/temp-download`;
 sh.exec(cmd5);
+sh.exec(cmd6);
+sh.exec(cmd7);
 console.log('deploy success');
+// 查看地址
+// https://appvantage.lshdan.xyz/temp-download/index.html 
 
